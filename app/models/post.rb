f154188752aac8eee104831deca22d7baec9ba31
Maria De Vuono <medevuono@gmail.com>
2017-07-14 20:45:32 -0500

    Profile routes and tweaks
    Add Confirmable behaviour in Devise for Users
    Add letter_opener gem for development
    Add Profile route, Controller, View and link
    Refactor Post partial, add Profilable and Linkable options

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,15 @@ class Post < ApplicationRecord
 
   #}
   #validate :count, numericality: {only_integer: true},
+
+  # loqe va despues de -> parametro es un landa 
+  #estoy ingresando en un simbolo yesterday el fragmento de codigo que se va a invocar cuando se llame yesterday
+  #scope :yesterday, (days) <- esto seria un paremetro
+  scope :yesterday, -> { where('created_at < ?', 1.days.ago) }
+  
+  after_create do |post| # esto es un hook
+    #logger.info "An email was sent to #{post.user.email}"
+    #PostMailer.post_published(post).deliver_now
+  end
+
 end
